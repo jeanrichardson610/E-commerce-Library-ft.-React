8bdc7f60809ed9e1b7e8b6daa9b53d43cdf156a3
jeanrichardson610 <jean.richardson610@gmail.com>
2025-08-20 11:30:48 -0700

    Testing

---
+++ src/components/Book.jsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import Ratings from "./ui/Ratings";
+import Price from "./ui/Price";
+
+const Book = ({ book }) => {
+  const [loaded, setLoaded] = useState(false);
+
+  
+
+ return (
+    <div className="book">
+      {!loaded && (
+        <div className="book__img--skeleton">
+          <div className="skeleton book__title--skeleton">
+            <div className="skeleton book__rating--skeleton">
+              <div className="skeleton book__price--skeleton"></div>
+            </div>
+          </div>
+        </div>
+      )}
+
+      <Link to={`/books/${book.id}`} className={!loaded ? "hidden" : ""}>
+        <figure className="book__img--wrapper">
+          <img
+            src={book.url}
+            alt={book.title}
+            className="book__img"
+            onLoad={() => setLoaded(true)}
+          />
+        </figure>
+      </Link>
+
+      {loaded && (
+        <>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Ratings rating={book.rating} />
+          <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
+        </>
+      )}
+    </div>
+  );
+};
+
+
+export default Book;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBars, faBolt, faBookOpen, faShoppingCart, faTags, faTimes, faStar, faStarHalf, faArrowLeft} from '@fortawesome/free-solid-svg-icons'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalf, faArrowLeft )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+<Router>
+  <Route>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Route>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
